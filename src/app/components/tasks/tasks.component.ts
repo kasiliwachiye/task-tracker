e45e0cc1b4cca98bde1b5261602d93550dff8e0f
Emmanuel Wachiye <wachiye25@gmail.com>
2021-11-14 23:35:37 +0300

    1 hr mark

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS;
+
+  constructor(private taskService: TaskService) { }
+
+  getTaks(): Task[] {
+    return TASKS;
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks => this.tasks = tasks); 
+  }
+
+}
